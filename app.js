5f1783f2fd0d45ecbe95dfe3499525daf7f1f97a
Stogniy Stanislav <stognijstas7@gmail.com>
2025-03-27 18:50:26 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,4 +2,31 @@ window.addEventListener("scroll", () => {
     const mainNav = document.querySelector(".main-header__navbar");
     mainNav.classList.toggle("active", window.scrollY > 0);
   });
-  
+  document.addEventListener('DOMContentLoaded', () => {
+    const testimonials = document.querySelectorAll('.testimonials-section__testimonial');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    let currentIndex = 0;
+
+    function updateTestimonials() {
+        testimonials.forEach((testimonial, index) => {
+            testimonial.classList.remove('active');
+            if (index === currentIndex) {
+                testimonial.classList.add('active');
+            }
+        });
+    }
+
+    prevBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+        updateTestimonials();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % testimonials.length;
+        updateTestimonials();
+    });
+
+    // Initialize the first testimonial as active
+    updateTestimonials();
+});
